763e5b3768b7cdb1e677b9954e1c1c6c85a9ee9c
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-07-30 23:46:33 +0530

    Added user profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule
