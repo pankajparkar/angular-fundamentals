56bdde99b7c55c4af5ac05fd9d3fd75401e422c1
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-07-30 23:53:43 +0530

    Added form input

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  
+  firstName: string;
+  lastName: string;
+  age: number;
+  hobbies: string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.firstName = 'Pankaj';
+    this.lastName = 'Parkar';
+    this.age = 27;
+    this.hobbies = ['Cricket', 'Football'];
   }
 
 }
